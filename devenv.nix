1845a395560e14967e79cc0f8007041432bf4fbb
Sandro Pratesi <sandro.pratesi@gmx.com>
2023-03-07 23:00:43 +0100

    chore(devenv): restructure repo with upgraded CI and tools

--- devenv.nix
+++ devenv.nix
@@ -5,8 +5,28 @@
     git
     openssh
     just
+    cargo-tarpaulin
+    cargo-audit
+    cargo-auditable
     cargo-insta # https://github.com/mitsuhiko/insta
+    codespell
+
+    # LSPs:
+    rust-analyzer
+    rnix-lsp
+    marksman
+    taplo
+    sqls
+    nodePackages.bash-language-server
+    nodePackages.yaml-language-server
   ];
+
+  enterShell = ''
+    # set -x  # for debugging
+    echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    echo "Run 'just' often, to prevent CI failures."
+    echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  '';
 
   # https://devenv.sh/languages/
   languages.rust.enable = true;
@@ -14,9 +34,7 @@
   # https://devenv.sh/pre-commit-hooks/
   # https://devenv.sh/reference/options/#pre-commit
   pre-commit.hooks = {
-    editorconfig-checker.enable = true;
     commitizen.enable = true; # https://www.conventionalcommits.org/en/v1.0.0/#summary
-    # other types: docs, refactor, chore, test, revert
 
     rustfmt.enable = true;
     cargo-check.enable = true;
@@ -28,7 +46,6 @@
     shfmt.enable = true;
     shellcheck.enable = true;
 
-    # markdownlint.enable = true;
     mdsh.enable = true; # execute example shell from Markdown files
     yamllint.enable = true;
     html-tidy.enable = true;
@@ -37,4 +54,3 @@
     yamllint.relaxed = true;
   };
 }
-
--- justfile
+++ justfile
@@ -1,17 +1,28 @@
 # <https://github.com/casey/just#quick-start>
 # <https://just.systems/man/en/>
 
-# set dotenv-load := true
-
-# Execute formatters and linters
-lint:
+# Run formatters, linters, tests and get code coverage
+test:
     cargo fmt
+    # Fix spelling errors with 'codespell -wi3' or add exceptions to .codespellrc
+    codespell
+    cargo check --all-features
+    cargo clippy --all-features -- --forbid unsafe_code
+    cargo check --tests
+    cargo clippy --tests
     pre-commit run --all-files
-    cargo clippy
+    cargo test
+    cargo doc --no-deps --all-features
+    cargo audit
+
+coverage:
+    cargo tarpaulin --skip-clean --target-dir target/coverage # to avoid recompilation
+
+# Build and open project documentation
+doc:
+    cargo doc --open --lib
 
 # Print help about 'just'
 help:
     @ just --list
     @ echo "More help: https://just.systems/man/en/chapter_22.html"
-
-# vim: set ft=make :
